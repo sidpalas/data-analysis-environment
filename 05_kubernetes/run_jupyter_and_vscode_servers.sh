96ff793951fa3bc1bd78b641743bee85bfec9f88
Sid Palas <sid.palas@gmail.com>
2019-09-21 16:48:03 -0700

    Added vscode server to image

---
+++ 05_kubernetes/run_jupyter_and_vscode_servers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#### Run VS Code server
+
+/code-server2.1523-vsc1.38.1-linux-x86_64/code-server &
+
+#### RUN Jupyter Server
+
+jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --allow-root /home/notebooks
